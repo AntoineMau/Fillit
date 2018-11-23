720c2b991bf5c57fda8d16d83db89f71cf522cd3
Julien DUMAY <judumay@student.42.fr>
2018-11-23 10:21:33 +0100

    test

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anmauffr <anmauffr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: judumay <judumay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 13:39:43 by anmauffr          #+#    #+#             */
-/*   Updated: 2018/11/22 17:48:47 by anmauffr         ###   ########.fr       */
+/*   Updated: 2018/11/23 10:20:28 by judumay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int		main(int ac, char **av)
 {
 	int		fd;
 	int		test;
+	char	*tmp;
 	char	*str;
 	int 	i;
 
@@ -24,10 +25,16 @@ int		main(int ac, char **av)
 	(void)ac;
 	if (!(fd = open(av[1], O_RDONLY)))
 		return (-1);
-	test = get_next_line(fd, &str);
+	test = 1;
+	str = (char*)malloc(sizeof(char) * 1000);
 	while (test > 0)
-		test = get_next_line(fd, &str);
-	ft_putstr(str);
+	{
+		test = get_next_line(fd, &tmp);
+		ft_strjoin(str, tmp);
+		dprintf(1, "tmp : %s\n", tmp);
+		free(tmp);
+	}
+	dprintf(1, "str : %s\n", str);
 	if (test == -1 || ft_verif(str) == -1)
 	{
 		printf("bad");
